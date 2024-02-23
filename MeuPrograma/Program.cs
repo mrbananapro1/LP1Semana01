2500c2cba85404733957c753c84497262d69919c
Leonardo Henrique <leonardojose@netcabo.pt>
2024-02-23 16:36:49 +0000

    Add bye world

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,6 +2,9 @@
 
 namespace MeuPrograma
 {
+/// <summary>
+/// This is a program that says Hello, World! and Bye, Wolrd!
+/// </summary>
     class Program
     {
         static void Main(string[] args)
