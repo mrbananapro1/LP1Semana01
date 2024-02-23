735590644e0e0c83d5bc5874cc7f8c9d8537c108
Leonardo Henrique <leonardojose@netcabo.pt>
2024-02-23 16:58:21 +0000

    Int Var

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 52;
+            Console.WriteLine(num);
+        }
+
+    }
+}
