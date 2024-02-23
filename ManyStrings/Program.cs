5fbda2d122f22cf98a04457559f59c3258044ca6
Leonardo Henrique <leonardojose@netcabo.pt>
2024-02-23 17:21:37 +0000

    Unicode Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string L1 = "\u24C1";
+            Console.WriteLine(L1);
+            string L2 = "\u24BA";
+            Console.WriteLine(L2);
+            string L3 = "\u24C4";
+            Console.WriteLine(L3);
+        }
+    }
+}
