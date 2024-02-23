be687a74c82dd1181cd65c89bc5c4cccee5759d1
Leonardo Henrique <leonardojose@netcabo.pt>
2024-02-23 17:57:40 +0000

    Input add

---
+++ AskForInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreve");
+            string escreve = Console.ReadLine();
+            int escreve1 = int.Parse(escreve);
+
+            Console.WriteLine("Escreve");
+            string escreve2 = Console.ReadLine();
+            float escreve3 = float.Parse(escreve2);
+
+            Console.WriteLine($"{escreve1 + escreve3}");
+
+        }
+    }
+}
