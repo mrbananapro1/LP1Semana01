3c6f098399bd2c30b689e5e1cabd98757761d015
Leonardo Henrique <leonardojose@netcabo.pt>
2024-02-23 16:45:44 +0000

    Add comentary

---
+++ DecimalHexa/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DecimalHexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert 4 whole numbers: ");
+            string n1 = Console.ReadLine();
+            
+        }
+    }
+}
--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -10,6 +10,7 @@ namespace MeuPrograma
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye, World");
         }
     }
 }
